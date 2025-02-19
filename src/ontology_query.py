7a72bc705fd09e7bfd01b2e506bb60057d81cf12
Rildojr6 <rildoajunior@fitec.org.br>
2025-02-19 20:38:28 -0300

    OAEPIC-41 Uso da ontologia na aplicacao

--- src/ontology_query.py
+++ src/ontology_query.py
@@ -1,4 +1,5 @@
 from rdflib import Graph
+from terms import readable_terms
 
 class OntologyQuery:
     def __init__(self, ontology_file: str):
@@ -7,14 +8,45 @@ class OntologyQuery:
 
     def query(self, command: str) -> str:
         # Example SPARQL query based on the command
-        sparql_query = """
+        # sparql_query = """
+        # SELECT ?subject ?predicate ?object
+        # WHERE {
+        #     ?subject ?predicate ?object.
+        # }
+        # """
+
+        words = command.split()
+        filter_conditions = []
+        for word in words:
+            filter_conditions.append(f"regex(?subject, '{word}', 'i')")
+            filter_conditions.append(f"regex(?predicate, '{word}', 'i')")
+            filter_conditions.append(f"regex(?object, '{word}', 'i')")
+
+        filter_expression = " || ".join(filter_conditions)
+
+        sparql_query = f"""
         SELECT ?subject ?predicate ?object
-        WHERE {
+        WHERE {{
             ?subject ?predicate ?object.
-        }
-        LIMIT 10
+            FILTER ({filter_expression})
+        }}
         """
+
+
+
         results = self.graph.query(sparql_query)
         response = "\n".join([f"{row.subject} {row.predicate} {row.object}" for row in results])
-        print(f'RESPOSTA: {response}')
+
+        for i, line in enumerate(response.split('\n')):
+            for uri, term in readable_terms.items():
+                line = line.replace(uri, term)
+            response = response.replace(response.split('\n')[i], line) # Atualiza a linha com os termos legíveis.
+
+
+
+
+
+
+
+        # print(f'RESPOSTA: <<<<{response}>>>>>')
         return response
